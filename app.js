05a797ab6a5ba956864af595482dfecd5d4fba91
The Feldmans <thefeldmans@Thes-MacBook-Pro.local>
2022-12-17 19:50:21 +0200

    files

---
+++ app.js
@@ -0,0 +1,201 @@
+var gameLoopInterval;
+var generateItemsTimeout;
+const pathSize = 24;
+const pathNum = 4;
+const numInPathPositionLeftRange = [36,39];
+
+var playerDirection = ""
+var animationAllowed = true;
+var aggregatedValue = 0;
+var moves = 0;
+var speedOfFallingFactor = 1;
+var gameScreenWidth;
+
+var currentPath = 0;
+
+var player;
+var playerBounding;
+const playerBottom = 35;
+var targetNumber;
+var gameScreenHeight; 
+
+function gameLoop() {
+    if(!animationAllowed || !player)
+        return;
+    
+    const currentPlayerLeft = parseFloat(player.style.left);
+    if(playerDirection && player) {
+        const toRight = playerDirection == "right";
+        if((!toRight && currentPlayerLeft >= 0) || (toRight && currentPlayerLeft <= 100)) 
+        {
+            player.style.left = toRight ? `${currentPlayerLeft + 5}%` : `${currentPlayerLeft - 5}%`;
+        }
+    }
+
+    document.querySelectorAll('.fallingNumber').forEach(function(button) {
+        var itemTop = parseFloat(button.style.top.slice(0,-1)) || 0;
+        itemTop = itemTop + speedOfFallingFactor + '%';
+        button.style.top = itemTop;
+        itemTop = parseFloat(button.style.top.slice(0,-1));
+
+        // if item overlap screen, remove it
+        if(itemTop > 100) {
+            button.remove();
+            return;
+        }
+
+        var itemLeft = (button.getBoundingClientRect().left * 100) / gameScreenWidth
+        playerBounding = player.getBoundingClientRect();
+        // todo: check collusion also when chaning path (left attr) !!!!
+        //else if((itemTop <= 100 - playerBottom && itemTop >= 100 - playerBottom - 12) 
+        if((itemTop <= 65 && itemTop >= 54)  && (itemLeft <= currentPlayerLeft + 15 && itemLeft > currentPlayerLeft)) {
+            console.log("hit!!! " + button.innerHTML)
+            moves++;
+            //speedOfFallingFactor += 0.3
+            calculateAggreatedValue(button.textContent);
+            button.remove();
+        }
+    });
+}
+
+function calculateAggreatedValue(value) {
+    const operator = value[0];
+    const numVal = parseInt(value.substring(1))
+    if(operator === "+")
+        aggregatedValue += numVal
+    else
+        aggregatedValue -= numVal
+        
+    calcAndPrintAggreatedValue(value);    
+}
+
+function calcAndPrintAggreatedValue(animationValue) {
+    if(aggregatedValue == targetNumber) {
+        alert("Win!!!")
+        alert("took you just " + moves + " moves")
+        reset();
+    }
+
+    document.getElementById("aggregatedValue").innerHTML = aggregatedValue
+    document.getElementById("currentVal").innerHTML = aggregatedValue
+    showAddedAnimation(animationValue)
+}
+
+function showAddedAnimation(value) {
+    document.getElementById("added").innerHTML = value
+    setTimeout(() => {
+        document.getElementById("added").innerHTML = ""
+    }, 1000)
+
+}
+
+function generateNewNumberItem() {
+    if(!animationAllowed)
+        return;
+
+  const rndInt = randomIntFromInterval(1, 10)
+  const toPath = randomIntFromInterval(1, pathNum)
+  const isPlus = randomIntFromInterval(1, 2) == 1
+
+  const htmlItem = `<div style="left: ${randomIntFromInterval(numInPathPositionLeftRange[0], numInPathPositionLeftRange[1])}%" 
+                        operator="${isPlus ? "plus" : "minus"}" 
+                        path="${toPath - 1}" 
+                        class='fallingNumber'>${isPlus ? "+" : "-"}${rndInt}<div>`
+  
+  // print to screen
+  document.getElementsByClassName('path')[toPath - 1].insertAdjacentHTML( 'beforeend', htmlItem );
+  const movesFactor = moves * 3;
+  
+  generateItemsTimeout = setTimeout(generateNewNumberItem, randomIntFromInterval(600 - movesFactor, 1200 - movesFactor));
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function stopAnimation() {
+    animationAllowed = false;
+}
+
+function keepAnimation() {
+    animationAllowed = true;
+}
+
+function generateNewTargetNumber() {
+    targetNumber = randomIntFromInterval(10, 30);
+    document.getElementById('target-num').innerHTML = targetNumber;
+}
+
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+        movePlayer("left");
+       // left arrow
+    }
+    else if (e.keyCode == '39') {
+        movePlayer("right");
+       // right arrow
+    }
+
+}
+
+function movePlayer(direction) {
+    player.setAttribute("path", direction);
+    // const left = (((pathIdx + 1) * pathSize) + ((pathIdx) * pathSize)) / 2
+    // player.style.left = `${left - 5}%`;
+    playerDirection = direction
+}
+
+function startup() {
+    generateNewTargetNumber();
+    player = document.getElementById("player");
+    player.style.bottom = playerBottom + "%"
+    player.setAttribute("top", player.getBoundingClientRect().top);
+    playerBounding = player.getBoundingClientRect();
+    gameScreenHeight = parseInt(document.getElementById('gameScreen').getBoundingClientRect().height);
+    gameScreenWidth = parseInt(document.getElementById('container').getBoundingClientRect().width);
+    const gameScreenXCenter = gameScreenWidth / 2;
+
+    document.onkeydown = checkKey;
+    document.addEventListener("visibilitychange", function() {
+        animationAllowed = !document.hidden;
+    });
+    document.addEventListener("touchstart", function(e) {
+        if(e.touches[0].clientX > gameScreenXCenter) {
+            checkKey({keyCode: '39'}) // right
+        }
+        else {
+            checkKey({keyCode: '37'}) // left
+        }
+    });
+}
+
+function reset() {
+    generateNewTargetNumber();
+    moves = 0;
+    aggregatedValue = 0;
+    document.querySelectorAll('.fallingNumber').forEach(i => i.remove()); 
+}
+
+function stop() {
+    animationAllowed = false
+    clearInterval(gameLoopInterval);
+    clearInterval(generateItemsInterval)
+}
+
+
+function startGame() {
+    setTimeout(startup, 200)
+    document.getElementById("welcome").classList.add("hidden");
+    gameLoopInterval = setInterval(gameLoop, 50);
+    generateItemsTimeout = setTimeout(generateNewNumberItem, 1000);
+}
